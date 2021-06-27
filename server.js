b6668f102d358dae9fdbdac9b3b2f1d862bdba90
augustinasjucas <augustinasjucas@gmail.com>
2021-06-27 13:32:37 +0300

    Pridėti griaučiai - padarytas HTML serveris su expressu ir socket.io (jei prireiks)

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = 3000;
+server.listen(process.env.PORT || port);
+
+/// KAD VEIKTŲ: 'npm install'
+
+console.log("Server is working!");
+
+app.get('/index', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+"socket.io": "*",
+"express": "*"
