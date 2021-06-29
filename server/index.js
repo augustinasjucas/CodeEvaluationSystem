4bd21ef14514ef7bbaa0f361a1e2ccd1a32ebfcb
augustinasjucas <augustinasjucas@gmail.com>
2021-06-29 19:00:45 +0300

    Changed to compiling only once instead of every time.

--- server/index.js
+++ server/index.js
@@ -32,48 +32,30 @@ function executeCommand (command) {                                             
 async function removeFile(path){                                                            // removes a file in `path` directory
     fs.unlink(path, function (err) {
         if (err) {
-            throw err;
+
         }
     });
 }
 async function createFile(path, value){                                                     // creates a file in `path` directory
     fs.writeFile(path, value, function (err) {
         if (err) {
-            throw err;
+
         }
     });
 }
-async function runTest(inputPath, code){                                                    // runs the test with given
+async function runTest(inputPath, execPath){                                                // runs the test with given
                                                                                             // test and returns
                                                                                             // {error, stdout, stderr}
 
-    var curNum = currentSubmissionNumber++;                                                 // in order not to get messed up, different
-                                                                                            // names are used
-
-                                                                                            // the name of cpp file
-    var codePath = path.join(__dirname, '/submissionsFolder/code' + curNum + '.cpp');       // the path to cpp file
-    var execPath = path.join(__dirname, '/submissionsFolder/' + curNum + '.out');           // the path to executable
-
-    await createFile(codePath, code);                                                       // creates the .cpp file with given code
-
-    //console.log('Compiling test with codeName: ' + curNum);
     var ret = {error: null, stdout: {}, stderr: {}};                                        // initializes the return value
-    await executeCommand('g++ -std=c++17 -o ' + execPath + ' ' + codePath).then((data) => { // compiles
-        if(data.error) ret = data;                                                          // if couldnt compile,this data will be returned
-    });
-    if(!ret.error){                                                                         // if compiled
-        await executeCommand('cat ' + inputPath + ' | ' + execPath).then((data) => {        // then excetute the code with this input
-            ret = data;                                                                     // and set the return data to the executed data
-        });
-    }
 
-    await removeFile(codePath);                                                             // after everything, remove used files
-    await removeFile(execPath);
+    await executeCommand('cat ' + inputPath + ' | ' + execPath).then((data) => {            // then excetute the code with this input
+        ret = data;                                                                         // and set the return data to the executed data
+    });
 
     return new Promise((resolve, reject) => {                                               // to be async, we return a promise
         resolve(ret);
     });
-
 }
 
 async function runCode(code, taskName){
@@ -82,15 +64,30 @@ async function runCode(code, taskName){
     if(!fileExists(pth)) return ["The task doesn't exist!"];                                // if it doesnt exist (this should never happen!)
     const info = require(pth);                                                              // gets the info.json.
 
-    var results = [];                                                                       // prepare to store all test results
+    var curNum = currentSubmissionNumber++;                                                 // different file names, for safety
+
+    var codePath = path.join(__dirname, '/submissionsFolder/code' + curNum + '.cpp');       // the path to cpp file
+    var execPath = path.join(__dirname, '/submissionsFolder/' + curNum + '.out');           // the path to executable
+
+    await createFile(codePath, code);                                                       // creates the cpp file
+    var results = []                                                                        // prepare to store all test results
+    await executeCommand('g++ -std=c++17 -o ' + execPath + ' ' + codePath).then((data) => { // compiles
+        if(data.error) results[0] = data;                                                   // if couldnt compile, this data will be returned
+    });
+    if(results[0]){
+        return results;
+    }
+
     for(var i = 1; i <= info.tests; i++){                                                   // loops through the tests
         const inpPath = path.join(__dirname, '/tasks/' + taskName + '/tests/input/' + i + '.in');
         const outPath = path.join(__dirname, '/tasks/' + taskName + '/tests/output/' + i + '.out');
         if(!fileExists(inpPath) || !fileExists(outPath)) return ["Wrong test data!"];       // this shouldn't ever happen!
-        let pb = runTest(inpPath, code);                                                    // runs the code with this test
+        let pb = runTest(inpPath, execPath);                                                // runs the code with this test
         results[i] = pb;                                                                    // stores the promise of the result in the array
     }
     await Promise.all(results);                                                             // wait until all results' promises are done
+    await removeFile(codePath);                                                             // after everything, remove used files
+    await removeFile(execPath);
     return results;
 }
 
