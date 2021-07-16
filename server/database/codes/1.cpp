b85705fa4a8ff0282661a265d460909a05f77cb8
Aldas25 <aldas.lenksas@gmail.com>
2021-07-16 17:10:47 +0300

    Added file for DB pool

--- server/database/codes/1.cpp
+++ server/database/codes/1.cpp
@@ -1,1 +1,10 @@
-input the code!
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    long long a, b; cin >> a >> b;
+    long long s = a + b;
+    cout << s << "\n";
+    return 0;
+}
---
+++ server/example_dbPool.js
@@ -0,0 +1,14 @@
+// this file is just an example. 
+// there should be a file, named 'dbPool.js', whose structure is identical to this one
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'your-password',
+    host: 'localhost',
+    port: 5432,
+    database: 'codeEvaluationDB'
+});
+
+module.exports = pool;
