864886a45634f67b834b4292e1c7c550705deb81
Aldas25 <aldas.lenksas@gmail.com>
2021-07-17 10:43:32 +0300

    Added DB functionality for submissions

--- server/dbAPI.js
+++ server/dbAPI.js
@@ -3,12 +3,42 @@ const path = require('path');
 const dbPool = require('./dbPool.js');
 
 var submissions = [];                                                       // temporary. has:
-var users =[{username: 'insertUsername', password: 'insertHashedPassword'}];// temporary. has: username(string), password(string)
+//var users =[{username: 'insertUsername', password: 'insertHashedPassword'}];// temporary. has: username(string), password(string)
 
 function getLastSubmissionNumber(){                                         // return the number of the last submission
+    // TODO: optimize a bit (so that only one query is required)
     return new Promise((resolve, reject) => {
+        // checking if there are any submissions
+        dbPool.query(
+            'SELECT * FROM submissions',
+            [],
+            (err, result) => {
+                if (err) reject();
+                const rows = result.rows;
+                console.log(' submissions row length: ' + rows.length);
+                if (rows.length == 0) resolve(0);
+                else {
+                    // if there are any submissions, getting the biggest index
+                    dbPool.query(
+                        'SELECT MAX(index) AS largestid FROM submissions',
+                        [],
+                        (err, result) => {
+                            if (err) reject();
+                            const rows = result.rows;
+                            //console.log('finding last submisssion number: ');
+                            //console.log(rows);
+                            const num = rows[0]['largestid'];
+                            //console.log(num);
+                            resolve(num);
+                        }
+                    );
+                }
+
+            }
+        );
+
         //////// to be replaced with db
-        resolve(0);
+        //resolve(0);
         ////////
     });
 }
@@ -20,21 +50,66 @@ function addSubmission(index, compiled, result, codePath, taskName){        // i
                                                                             // taskName - the shortname to the task
     //////// to be replaced db
     submissions[index] = {compiled: compiled, result: result, codePath: codePath, taskName: taskName};
+    //console.log('add submisssion, submission: ');
+    //console.log(submissions[index]);
+
+   // console.log('inserting, add submission');
+
+   // TODO: also add user id ; do not add index (make it auto increment)
+    dbPool.query(
+        `INSERT INTO submissions (index, compiled, result, codepath, taskname)
+         VALUES ($1, $2, $3, $4, $5)`,
+        [index, compiled, JSON.stringify(result), codePath, taskName],
+        (err, result) => {
+            if (err) {
+                console.log("error occured: ");
+                console.log(err);
+             //   reject();
+            }
+           // console.log("inserted");
+            const rows = result.rows;
+            //console.log("rows: ");
+            //console.log(rows);
+            // const rows = result.rows;
+            // if (rows.length == 0) resolve(false);
+            // resolve(true);
+        }
+    );
+
     ////////
 
 }
 function findIfSubExists(index){                                            // finds whether submission `index` is in DB and returns true/false
     return new Promise((resolve, reject) => {
-        //////// to be replaced with db
-        if(submissions[index]) resolve(true);
-        else resolve(false);
-        ////////
+
+        dbPool.query(
+            'SELECT * FROM submissions WHERE index=($1)',
+            [index],
+            (err, result) => {
+                if (err) reject();
+                const rows = result.rows;
+                if (rows.length == 0) resolve(false);
+                resolve(true);
+            }
+        );
     });
 }
 function getSubResult(index){                                               // returns the result of `index` submission
     return new Promise((resolve, reject) => {
+        dbPool.query(
+            'SELECT * FROM submissions WHERE index=($1)',
+            [index],
+            (err, result) => {
+                if (err) reject();
+                const rows = result.rows;
+                console.log('getting sub result');
+                console.log(rows);
+                resolve(rows);
+            }
+        );
+
         //////// to be replaced with db
-        resolve(submissions[index]);
+        //resolve(submissions[index]);
         ////////
     });
 }
--- server/db_scheme.sql
+++ server/db_scheme.sql
@@ -1,7 +1,21 @@
 --CREATE DATABASE codeevaluationdb;
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS submissions;
+
 CREATE TABLE users(
     id         SERIAL PRIMARY KEY,
     username   VARCHAR(100) NOT NULL,
     password   VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, password) 
+    VALUES ('insertUsername', 'insertHashedPassword'); -- temporary user
+
+CREATE TABLE submissions(
+    index      INT NOT NULL,
+    compiled   BOOLEAN NOT NULL,
+    result     json NOT NULL,
+    codepath   VARCHAR(100) NOT NULL,
+    taskname   VARCHAR(100) NOT NULL
 );
